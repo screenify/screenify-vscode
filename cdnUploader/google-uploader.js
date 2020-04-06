783b496a51da51abd85ae59af8a9626e4491137a
AdamMomen <adammuman81@gmail.com>
2020-04-06 17:24:57 +0100

    bugfix

--- cdnUploader/google-uploader.js
+++ cdnUploader/google-uploader.js
@@ -5,43 +5,50 @@ const {
 const path = require('path')
 const serviceKey = path.join(__dirname, './keys.json')
 const randomstring = require("randomstring");
-
+const {
+    format
+} = require('util')
 
 // // const bucket = storage.bucket('screenify_bucket')
 module.exports = class GoogleUploader {
     constructor(config) {
-        storage = new Storage({
+        this.storage = new Storage({
             keyFilename: serviceKey,
-            // config("googleServiceKey"),
             projectId: config.get("googleProjectId")
-            // 'careful-voyage-273218',
         })
+        this.bucketName = config.get("googleBucketName")
     }
     upload(buffer) {
-        let bucket = this.storage.bucket(config.get("googleBucketName"))
-        bucket.acl.default.add({
-            entity: "allUsers",
-            role: "READER",
-        }, function (res, err) {
-            if (err) console.log(err)
-            else conosle.log(res)
-        })
+        let bucket = this.storage.bucket(this.bucketName)
+        // bucket.acl.default.add({
+        //     entity: "allUsers",
+        //     role: "READER",
+        // }, function (res, err) {
+        //     if (err) console.log(err)
+        //     else conosle.log(res)
+        // })
         return new Promise((resolve, reject) => {
-            const blob = bucket.file(`${randomstring.generate(6)}.png`.replace(/ /g, "_"))
+            const filename = `${randomstring.generate(6)}.png`;
+            const blob = bucket.file(filename)
+            console.log(blob.name)
             const blobStream = blob.createWriteStream({
                 metadata: {
+                    "Content-Type": "image/png",
+                    "Content-Disposition": 'inline;filename="filename.png"',
                     cacheControl: "public, max-age=6000"
                 },
                 public: true,
                 resumable: false
             })
             blobStream.on('finish', () => {
-                    const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`
-
+                    const publicUrl = format(
+                        `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+                    )
+                    console.log(publicUrl)
                     resolve(publicUrl)
                 })
                 .on('error', (error) => {
-                    reject(`Unable to upload image, something went wrong: ${error}`)
+                    reject(error.message)
                 })
                 .end(buffer)
         })
